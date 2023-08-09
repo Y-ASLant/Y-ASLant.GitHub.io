6becd7fcfc8d5ae8ea88991d3ac2fc2f757b65e3
ASLant <77436463+Y-ASLant@users.noreply.github.com>
2023-08-09 11:32:43 +0800

    1

--- fk/conn/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db=array(
-	"servername" => "127.0.0.1",
-	"username" => "be49s2pt4",
-	"password" => "0v8g2lxp",
-	"dbname" => "be49s2pt4"
-);
-?>
--- fk/shop/return.php
+++
@@ -1,6 +0,0 @@
-<?php
-require '../conn/conn.php';
-require '../conn/function.php';
-
-require 'template/'.$C_shopt.'/return.php';
-?>
