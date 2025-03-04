eb10110e7eb2d9c0c147ad8836ca84fc0010d30f
ASLant <77436463+Y-ASLant@users.noreply.github.com>
2025-03-04 10:02:20 +0800

    Update config.ts

--- Note/docs/.vuepress/config.ts
+++ Note/docs/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from 'vuepress'
 import { plumeTheme } from 'vuepress-theme-plume'
 
 export default defineUserConfig({
-  base: '/',
+  base: '/Note/',
   lang: 'zh-CN',
   title: 'ASLant',
   description: '',
